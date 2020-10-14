f0fb6a4afa9e64af2a40ae8148a910e6951d0470
Maxwell Wangler <mwangler@iastate.edu>
2020-10-14 14:25:13 -0500

    Finished_assessment

--- Node.JS/src/app.js
+++ Node.JS/src/app.js
@@ -1,13 +1,187 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const fs = require("fs");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
    res.send('You keep using that word. I do not think it means what you think it means.');
 });
 
+//adds an order to orders.json. I made each order have an id, first and last name, Id of item, and number of item.
+app.post('/addOrder', function (req, res) {
+	
+	//Grabs the non-orderId values from the json input.
+	var lastName = req.body.lastName;
+	var firstName = req.body.firstName;
+	var itemId = req.body.itemId;
+	var numberOfItems = req.body.numberOfItems;
+	
+	//checks to see if the Last name and First name belong to a customer from customers.json
+	var entered = 0;
+	fs.readFile( __dirname + "/../data/customers.json", 'utf8', function (err, data) {
+		data = JSON.parse( data );
+		val = 0;
+		for(val = 0; val < Object.keys(data).length; val++){
+			if(data[val].firstName === firstName && data[val].lastName === lastName)
+			{
+				entered = 1;
+				//adds the new data to the orders.json file
+				fs.readFile( __dirname + "/../data/orders.json", 'utf8', function (err, prevData) {
+					//if the file isn't empty
+					if(prevData.length > 0) {
+						prevData = JSON.parse( prevData );
+						const i = Object.keys(prevData).length + 1;
+						var min = 10000;
+						var max = 99999;
+						var num = Math.floor(Math.random() * (max - min + 1)) + min;
+						var newEntry = 
+						{
+							"orderId" : num + '',
+							"lastName" : lastName,
+							"firstName" : firstName,
+							"itemId" : itemId,
+							"numberOfItems" : numberOfItems
+						}
+						prevData[i] = newEntry;
+						fs.writeFile(__dirname + "/../data/orders.json", JSON.stringify(prevData), 'ascii', function (err, d) {
+							console.log(prevData);
+							res.end(JSON.stringify(prevData));
+						});
+					} else {
+						//if the file is empty
+						var min = 10000;
+						var max = 99999;
+						var num = Math.floor(Math.random() * (max - min + 1)) + min;
+						var newEntry = {
+							1 : {
+								"orderId" : num + '',
+								"lastName" : lastName,
+								"firstName" : firstName,
+								"itemId" : itemId,
+								"numberOfItems" : numberOfItems
+							}
+						}
+						prevData = newEntry;
+						fs.writeFile(__dirname + "/../data/orders.json", JSON.stringify(prevData), 'ascii', function (err, d) {
+							console.log(prevData);
+							res.end(JSON.stringify(prevData));
+						});
+					}
+				});
+			} else if(val === Object.keys(data).length -1 && entered === 0) {
+				//Returns the unchanged orders.json 
+				fs.readFile( __dirname + "/../data/orders.json", 'utf8', function (err, prevData) {
+					console.log(JSON.parse(prevData));
+					res.end(prevData);
+				});
+			}
+		}
+	});
+});
+
+//updates order from id
+app.put('/updateOrder', (req,res) => {
+	var orderId = req.body.orderId;
+	var lastName = req.body.lastName;
+	var firstName = req.body.firstName;
+	var itemId = req.body.itemId;
+	var numberOfItems = req.body.numberOfItems;
+	var entered = 0;
+	//checks to see if the new name is in customers.json
+	fs.readFile( __dirname + "/../data/customers.json", 'utf8', function (err, data) {
+		data = JSON.parse( data );
+		val = 0;
+		for(val = 0; val < Object.keys(data).length; val++){
+			if(data[val].firstName === firstName && data[val].lastName === lastName)
+			{
+				entered = 1;
+				//checks for the orderId in orders.json then updates it.
+				fs.readFile( __dirname + "/../data/orders.json", 'utf8', function (err, prevData)
+				{
+					prevData = JSON.parse( prevData );
+					if(Object.keys(prevData).length > 0) {
+						var i = 1;
+						for(i = 1; i <= Object.keys(prevData).length; i++)
+						{
+							var enter =0;
+							if(prevData[i].orderId + '' === orderId + '')
+							{
+								enter = 1;
+								var newEntry = 
+								{
+									"orderId" : orderId,
+									"lastName" : lastName,
+									"firstName" : firstName,
+									"itemId" : itemId,
+									"numberOfItems" : numberOfItems
+								}
+								prevData[i] = newEntry;
+								fs.writeFile(__dirname + "/../data/orders.json", JSON.stringify(prevData), 'ascii', function (err, d) {
+									console.log(prevData);
+									res.end(JSON.stringify(prevData));
+								});
+							} else if(i === Object.keys(prevData).length && enter === 0) {
+								console.log(prevData);
+								res.end(JSON.stringify(prevData));
+							}
+						}
+					} else {
+						//returns the unchanged data
+						console.log(prevData);
+						res.end(JSON.stringify(prevData));
+					} 
+				});
+			} else if(val === Object.keys(data).length -1 && entered === 0) {
+				//returns the unchanged data
+				fs.readFile( __dirname + "/../data/orders.json", 'utf8', function (err, prevData) {
+					console.log(JSON.parse(prevData));
+					res.end(prevData);
+				});
+			}
+		}
+	});
+});
+//deletes an order with orderId
+app.delete('/cancelOrder', (req, res) => {
+	var orderId = req.body.orderId;
+	//builds a new json list with all elements but the one with orderId
+	fs.readFile( __dirname + "/../data/orders.json", 'utf8', function (err, data)
+	{
+		data = JSON.parse( data );
+		i = 1;
+		j = 1;
+		found = 0;
+		var newData = {};
+		startingSize = Object.keys(data).length;
+		for(i = 1; i <= startingSize; i++){
+			if(!(data[i].orderId === orderId)) {
+				newData[j] = data[i];
+				j++;
+			}
+			//returns the build json object
+			if(i === startingSize) {
+				fs.writeFile(__dirname + "/../data/orders.json", JSON.stringify(newData), 'ascii', function (err, d) {
+					console.log(newData);
+					res.end(JSON.stringify(newData));
+				});	
+			}
+		}
+	});
+});
+//reads the orders.json file and returns it
+app.get('/listAll', (req, res) => {
+	fs.readFile( __dirname + "/../data/orders.json", 'utf8', function (err, data) {
+		console.log( data );
+		res.end(data);
+	});
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
 
 module.exports = app;
--- Node.JS/src/tests/app.test.js
+++ Node.JS/src/tests/app.test.js
@@ -1,5 +1,7 @@
+//Ran out of time, couldn't get full test coverage of edge cases but each endpoint is tested.
 const request = require('supertest');
 const app = require('../app');
+const fs = require("fs");
 
 test('Should return 200 status', async () => {
     const response = await request(app).get('/health');
@@ -9,4 +11,73 @@ test('Should return 200 status', async () => {
 test('Test response text', async () => {
     const response = await request(app).get('/health');
     expect(response.text).toBe('You keep using that word. I do not think it means what you think it means.');
-})
+});
+
+//tests if the correct values are returned
+test('Test listAll', async () => {
+	const response = await request(app).get('/listAll');
+	var json_response = JSON.parse(response.text);
+	expect(json_response[1].lastName + ' ' + json_response[1].firstName + ' ' + json_response[1].itemId + ' ' + json_response[1].numberOfItems).toBe('Rogers Steve 085 2');
+	expect(json_response[2].lastName + ' ' + json_response[2].firstName + ' ' + json_response[2].itemId + ' ' + json_response[2].numberOfItems).toBe('Danvers Carol 009 10');
+	expect(json_response[3].lastName + ' ' + json_response[3].firstName + ' ' + json_response[3].itemId + ' ' + json_response[3].numberOfItems).toBe('Stark Tony 750 1');
+});
+
+//adds a new order, checks to see if it correct, recordeds the orderId and then deletes it
+test('Test addOrder', async () => {
+	const data = {
+		"lastName": "Rogers",
+		"firstName": "Steve",
+		"itemId": "085",
+		"numberOfItems": "2",
+	}
+	const response = await request(app).post('/addOrder').send(data);
+	var json_response = JSON.parse(response.text);
+	expect(json_response[4].lastName + ' ' + json_response[4].firstName + ' ' + json_response[4].itemId + ' ' + json_response[4].numberOfItems).toBe('Rogers Steve 085 2');
+	const undoData = {
+		"orderId" : json_response[4].orderId,
+	}
+	await request(app).delete('/cancelOrder').send(undoData);
+});
+
+//updates an order, checks it and then changes it back.
+test('Test updateOrder', async () => {
+	const data = {
+		"orderId": "40000",
+		"lastName": "Danvers",
+		"firstName": "Carol",
+		"itemId": "705",
+		"numberOfItems": "5",
+	}
+	const undoData = {
+		"orderId": "40000",
+		"lastName": "Rogers",
+		"firstName": "Steve",
+		"itemId": "085",
+		"numberOfItems": "2",
+	}
+	const response = await request(app).put('/updateOrder').send(data);
+	var json_response = JSON.parse(response.text);
+	expect(json_response[1].lastName + ' ' + json_response[1].firstName + ' ' + json_response[1].itemId + ' ' + json_response[1].numberOfItems).toBe('Danvers Carol 705 5');
+	await request(app).put('/updateOrder').send(undoData);
+});
+
+//adds a new order, checks to make sure it was add, deletes it and then checks if it is back to the orgininal state.
+test('Test cancelOrder', async () => {
+	expected = await request(app).get('/listAll');
+	json_expected = expected.text;
+	const dataToAdd = {
+		"lastName": "Danvers",
+		"firstName": "Carol",
+		"itemId": "705",
+		"numberOfItems": "5",
+	}
+	const added = await request(app).post('/addOrder').send(dataToAdd);
+	json_added = JSON.parse(added.text);
+	expect(added.text === expected.text).toBe(false);
+	const data = {
+		"orderId" : json_added[4].orderId,
+	}
+	const response = await request(app).delete('/cancelOrder').send(data);
+	expect(response.text).toBe(json_expected);
+	
+});
